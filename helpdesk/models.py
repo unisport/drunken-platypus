29606f447f59b8069bfa3008f0da3d116c6dc57b
Bad ass like Fadur! <kn@unisport.dk>
2016-10-13 12:27:35 +0000

    Housekeeping

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1,5 +1,27 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Issue(models.Model):
+    OPEN = 'OPEN'
+    RESOLVED = 'RESOLVED'
+    REOPENED = 'REOPENED'
+    ONHOLD = 'ONHOLD'
+    
+    STATUS_CHOICES = (
+        (OPEN, 'Open'),
+        (RESOLVED, 'Resolved'),
+        (REOPENED, 'Reopened'),
+        (ONHOLD, 'On Hold')
+    )
+
+    title = models.CharField(max_length=255)
+    summary = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # FIXME: Should be logged in user
+    author = models.CharField(max_length=100)
+    kb = models.BooleanField(default=False)
+    status = models.CharField(
+        choices = STATUS_CHOICES,
+        default = OPEN,
+        max_length = 100
+    )
